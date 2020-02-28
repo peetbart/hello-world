2ddbfa8003fe569eaa9c53bf4563d010d96481c2
Peter Bartfay <peter.bartfay@weareburrows.com>
2020-02-28 15:51:04 +0000

    refactor

--- distance_formulas/euclidean_distance.py
+++ distance_formulas/euclidean_distance.py
@@ -2,16 +2,9 @@ import math
 
 def euclidean_distance(pt1, pt2):
     distance = 0
-    squared = 0
-    count = 0
-    for point_a in pt1:
-        squared += ((point_a - pt2[count])**2)
-        count += 1
-
-    distance += squared
-    distance = math.sqrt(distance)
-
-    return distance
+    for i in range(len(pt1)):
+      distance += (pt1[i] - pt2[i]) ** 2
+    return math.sqrt(distance)
 
 print(euclidean_distance([1,2], [4,0]))
 print(euclidean_distance([5,4,3], [1,7,9]))
